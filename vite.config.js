8d17311135f140ffe3ef34d39c659184dbeb001e
Ezequiel Caviglione <ezecaviglione@gmail.com>
2026-06-30 15:14:40 -0300

    Deploy settings

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/landing-verduleria/',
     plugins: [
         react(),
         tailwindcss(),
